7d51f790457533c0e24ee097c261f2f38676ce2e
Charles Zambrano <charleszambranoespe@outlook.com>
2024-06-14 11:28:53 -0500

    [ADD] CrossOrigin notation to all controllers

--- src/main/java/com/banquito/core/interest/controller/InterestRateLogController.java
+++ src/main/java/com/banquito/core/interest/controller/InterestRateLogController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST,
+        RequestMethod.PUT })
 @RestController
 @RequestMapping(path = "/interest-rate-logs")
 public class InterestRateLogController {
